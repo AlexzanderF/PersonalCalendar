44fb4d8a9cd86b852e0bedf509a38190578f0b34
AlexanderF <alex58020@gmail.com>
2022-05-19 11:29:06 +0300

    Add enumerators

---
+++ PersonalCalendar/Enums.h
@@ -0,0 +1,17 @@
+#pragma once
+enum UpdateOption {
+	Title = 1,
+	Comment = 2,
+	DateOpt = 3,
+	StartTime = 4,
+	EndTime = 5,
+};
+enum MenuAction{
+	eAddEvent = 1,
+	eCancelEvent = 2,
+	eDailySchedule = 3,
+	eUpdateEvent = 4,
+	eSearch = 5,
+	eFindFreeTime = 6,
+	eGetReport = 7
+};
